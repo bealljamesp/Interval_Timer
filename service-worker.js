a8ef7227d27d9d214d38d59bdeb5d1c6d211de74
James <jamespairepo@gmail.com>
2025-09-09 01:35:50 -0700

    v11

--- service-worker.js
+++ service-worker.js
@@ -3,3 +3,10 @@ const CACHE_NAME = `boxing-timer-${CACHE_VERSION}`;
 const ASSETS = ['.', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim(); // <- important
+  })());
+});
